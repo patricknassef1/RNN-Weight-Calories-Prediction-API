417f38c590f8e538c43c0ed6af6edd1bce19e52d
Patrick Nassef <132225536+patricknassef1@users.noreply.github.com>
2025-03-03 21:00:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ def home():
     return "API is running", 200  # Plain text response for health checks
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=False)  
+    app.run(debug=False, host="0.0.0.0", port=5000)
+
+
 
